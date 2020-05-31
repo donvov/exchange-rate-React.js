5c537744c37836b8a730c65bf1a0e2721fb68449
Donvov <donvov1990@gmail.com>
2020-05-31 18:58:35 +0300

    my first commit

---
+++ src/about/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css';
+
+
+class About extends React.Component {
+
+    render() {
+        return (
+            <div className="about">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consectetur deserunt esse hic nemo omnis optio repellat reprehenderit velit. Accusamus architecto, dolorem doloremque error libero nesciunt nisi quidem quisquam recusandae.</p>
+            </div>
+        );
+    }
+}
+
+
+export default About;
